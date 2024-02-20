bbc015e102b76a916cb3498070fe4c51ec989b81
VISH01AL <g.vish1al@gmail.com>
2024-02-19 21:25:09 -0500

    new data

--- src/components/MainPage/GenreMovies.jsx
+++ src/components/MainPage/GenreMovies.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import './GenreMovie.css'
+
+
 export const GenreMovies = ({movies,genre}) => {
-    console.log(movies)
-    const actionMovies = movies.filter(movie => movie.genre.includes(genre));
+    
+    const actionMovies = movies.filter(movie => movie.Genre.includes(genre));
 
   return (
     <div>
       <h1>{genre} Movies</h1>
       <div className="movies-container">
-        {actionMovies.map(movie => (
-          <div key={movie.id} className="movie-card">
+        {actionMovies.map((movie,index) => (
+          <div key={index} className="movie-card">
             <img src={movie.Poster_Link} alt={movie.Series_Title} />
             <h2>{movie.Series_Title}</h2>
           </div>
