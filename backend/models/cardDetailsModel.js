b4769a927bdc189949b1372c25cd2957810bb747
VISH01AL <g.vish1al@gmail.com>
2024-03-27 11:29:58 -0400

    final commit

--- backend/models/cardDetailsModel.js
+++ backend/models/cardDetailsModel.js
@@ -40,17 +40,17 @@ const CardDetail = sequelize.define('CardDetail', {
 });
 
 // Sync the model with the database
-async function syncModel() {
-  try {
-    await CardDetail.sync({ alter: true });
-    console.log("CardDetail model synchronized successfully.");
-  } catch (error) {
-    console.error("Error synchronizing CardDetail model:", error);
-  }
-}
+// async function syncModel() {
+//   try {
+//     await CardDetail.sync({ alter: true });
+//     console.log("CardDetail model synchronized successfully.");
+//   } catch (error) {
+//     console.error("Error synchronizing CardDetail model:", error);
+//   }
+// }
 
 
-syncModel();
+// syncModel();
 
 // Export the CardDetail model for use in other parts of the application
 module.exports = CardDetail;
